48a52a194c2e06b7c47bb8e1a2a5c3ceb2069582
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-14 12:21:03 -0700

    add in useState for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,54 @@
 // Nav.js
 
 import React from 'react';
+import React, { useState } from "react";
 import logo from "../images/Logo.svg"
 
 function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <nav>
         <a href="/">
             <img src={logo} alt="logo" />
         </a>
 
+        {/* mobile navbar */}
         <div>
           <div className='bar'></div>
           <div className='bar'></div>
           <div className='bar'></div>
         </div>
+
+        {/* nav items */}
+        <ul>
+          <li>
+            <a href='/'>Home</a>
+          </li>
+          <li>
+            <a href='/'>About</a>
+          </li>
+          <li>
+            <a href='/'>Services</a>
+          </li>
+          <li>
+            <a href='/'>Menu</a>
+          </li>
+          <li>
+            <a href='/'>Reservations</a>
+          </li>
+          <li>
+            <a href='/'>Order Online</a>
+          </li>
+          <li>
+            <a href='/'>Login</a>
+          </li>
+        </ul>
+
     </nav>
   );
 }
