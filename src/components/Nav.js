0b94bef7d4c5a7d54d1f2bf4131a652b6ae1a995
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-14 12:38:50 -0700

    fix error regarding single quote instead of backticks in code for nav to work properly

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,20 +11,20 @@ function Nav() {
   }
 
   return (
-    <nav className='navbar'>
-        <a href="/">
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <a href="/" className="logo">
             <img src={logo} alt="logo" />
         </a>
 
         {/* mobile navbar */}
-        <div>
+        <div className="menu-icon" onClick={toggleMenu}>
           <div className='bar'></div>
           <div className='bar'></div>
           <div className='bar'></div>
         </div>
 
         {/* nav items */}
-        <ul>
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
           <li>
             <a href='/'>Home</a>
           </li>
