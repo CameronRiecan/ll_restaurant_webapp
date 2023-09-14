0a10b9275c5464627fb0818fcedc0b37e11b290c
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-14 16:54:05 -0700

    fix localhost issue, comment out link until bug fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 // Header.js
 
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
-      {/* header content goes here */}
-      Header
+    <header className='header'>
+      <section>
+        {/* banner texts */}
+        <div className='banner'>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          {/* <Link to="/Booking"><button aria-label='Reserve a Table'>Reserve Table</button></Link> */}
+        </div>
+      </section>
     </header>
   );
 }
