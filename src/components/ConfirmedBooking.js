42e7f8787cfd08b32fe6a318ae2dfb78ef6544d8
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-16 12:42:29 -0700

    add confirmed booking component file

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,13 @@
+// ConfirmedBooking.js
+
+import React from 'react';
+
+function ConfirmedBooking() {
+  return (
+    <main>
+      {/* main content goes here */}
+    </main>
+  );
+}
+
+export default ConfirmedBooking;
