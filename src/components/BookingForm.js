bae701bbb4619abb67a8b25feaa71ae3160c0bef
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-18 10:22:26 -0700

    add dropdown menu for number of guest selection on form with min of 1 and max of 8

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,5 +1,3 @@
-// BookingForm.js
-
 import React, { useState } from 'react';
 
 function BookingForm(props) {
@@ -11,7 +9,6 @@ function BookingForm(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         props.SubmitForm(e);
-
     }
 
     const handleChange = (e) => {
@@ -19,53 +16,58 @@ function BookingForm(props) {
         props.dispatch(e);
     }
 
-  return (
-    <header>
-        <section>
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <div>
-                        <label htmlFor='book-date'>Choose Date:</label>
-                        <input id='book-date' value={date} onChange={(e) => handleChange(e.target.value)} type='date' required/>
-                    </div>
+    return (
+        <header>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <fieldset>
+                        <div>
+                            <label htmlFor='book-date'>Choose Date:</label>
+                            <input id='book-date' value={date} onChange={(e) => handleChange(e.target.value)} type='date' required />
+                        </div>
 
-                    {/* time selection */}
-                    <div>
-                        <label htmlFor='book-time'>Choose Time:</label>
-                        <select id='book-time' value={times} onChange={(e) => setTimes(e.target.value)}>
-                            <option value=''>Select a Time</option>
-                            {
-                                props.availableTimes.availableTimes.map(availableTimes => {return <option key={availableTimes}>{availableTimes}</option>})
-                            }
-                        </select>
-                    </div>
+                        {/* time selection */}
+                        <div>
+                            <label htmlFor='book-time'>Choose Time:</label>
+                            <select id='book-time' value={times} onChange={(e) => setTimes(e.target.value)}>
+                                <option value=''>Select a Time</option>
+                                {
+                                    props.availableTimes.availableTimes.map(availableTimes => { return <option key={availableTimes}>{availableTimes}</option> })
+                                }
+                            </select>
+                        </div>
 
-                    {/* number of guests */}
-                    <div>
-                        <label htmlFor='book-guests'>Number of Guests:</label>
-                        <input id='book-guests' min={1} value={guests} onChange={(e) => setGuests(e.target.value)} />
-                    </div>
+                        {/* number of guests */}
+                        <div>
+                            <label htmlFor='book-guests'>Number of Guests:</label>
+                            <select id='book-guests' value={guests} onChange={(e) => setGuests(e.target.value)}>
+                                {[...Array(8).keys()].map((num) => (
+                                    <option key={num + 1} value={num + 1}>
+                                        {num + 1}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
 
-                    {/* occasion field */}
-                    <div>
-                        <label htmlFor='book-occasion'>Occasion:</label>
-                        <select id='book-occasion' key={occasion} value={occasion} onChange={(e) => setOccasion(e.target.value)}>
-                            <option>Birthday</option>
-                            <option>Anniversary</option>
-                            <option>Other</option>
-                        </select>
-                    </div>
+                        {/* occasion field */}
+                        <div>
+                            <label htmlFor='book-occasion'>Occasion:</label>
+                            <select id='book-occasion' key={occasion} value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+                                <option>Birthday</option>
+                                <option>Anniversary</option>
+                                <option>Other</option>
+                            </select>
+                        </div>
 
-                    {/* submit button */}
-
-                    <div className='btnReceive'>
-                        <input aria-label='Submit' type='submit' value={"Make Your Reservation"} />
-                    </div>
-                </fieldset>
-            </form>
-        </section>
-    </header>
-  );
+                        {/* submit button */}
+                        <div className='btnReceive'>
+                            <input aria-label='Submit' type='submit' value={"Make Your Reservation"} />
+                        </div>
+                    </fieldset>
+                </form>
+            </section>
+        </header>
+    );
 }
 
 export default BookingForm;
