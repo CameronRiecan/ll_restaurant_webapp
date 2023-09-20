65657c75088c19719dff89dd468cedcbfa69fab2
Cameron <124470971+CameronRiecan@users.noreply.github.com>
2023-09-19 18:39:48 -0700

    add accessability updates to menu.js component with aria labelsto work with template literals

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,21 +8,21 @@ function Menu() {
     <div className='menu-container'>
       <div className='menu-header'>
         <h2>This week's specials!</h2>
-        <button>Order Menu</button>
+        <button aria-label='Order Menu'>Order Menu</button>
       </div>
 
       {/* menu cards */}
       <div className='cards'>
         {
             recipes.map(recipe => <div key={recipe.id} className='menu-items'>
-                <img src={recipe.image} alt='' />
+                <img src={recipe.image} alt={`Image of ${recipe.title}`} />
                 <div className='menu-content'>
                     <div className='heading'>
                         <h5>{recipe.title}</h5>
                         <p>{recipe.price}</p>
                     </div>
                     <p>{recipe.description}</p>
-                    <button className='orderbtn'>Order Now!</button>
+                    <button className='orderbtn' aria-label={`Order ${recipe.title}`}>Order Now!</button>
                 </div>
             </div>)
         }
